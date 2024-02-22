15bf18fca79a6e885703ae995372154ea98867f0
egerin <zhenya.eww@gmail.com>
2024-02-22 13:20:37 +0300

    add version 0.0.6

--- src/main/java/com/example/laba/service/CatService.java
+++ src/main/java/com/example/laba/service/CatService.java
@@ -30,31 +30,28 @@ public class CatService implements Services {
     @Override
     public void saveInfCat(String action) throws JsonProcessingException {
         String json = getInf(action);
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode jsonNode = mapper.readValue(json, JsonNode.class);
-            JsonNode dataArray = jsonNode.get("data");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readValue(json, JsonNode.class);
+        JsonNode dataArray = jsonNode.get("data");
 
-            for (JsonNode catNode : dataArray) {
-                if (action.equals("breeds")) {
-                    String breed = catNode.get("breed").asText();
-                    String country = catNode.get("country").asText();
-                    String origin = catNode.get("origin").asText();
-                    String coat = catNode.get("coat").asText();
-                    String pattern = catNode.get("pattern").asText();
-                    Cat cat = new Cat(breed, country, origin, coat, pattern);
-                    catRepository.saveCat(cat);
-                } else if (action.equals("facts")) {
-
-                    String fact = catNode.get("fact").asText();
-                    int length = catNode.get("length").asInt();
-                    CatFact catFact = new CatFact(fact, length);
-                    catInfRepository.saveCatFact(catFact);
-                }
+        for (JsonNode catNode : dataArray) {
+            if (action.equals("breeds")) {
+                String breed = catNode.get("breed").asText();
+                String country = catNode.get("country").asText();
+                String origin = catNode.get("origin").asText();
+                String coat = catNode.get("coat").asText();
+                String pattern = catNode.get("pattern").asText();
+                Cat cat = new Cat(breed, country, origin, coat, pattern);
+                catRepository.saveCat(cat);
+            } else if (action.equals("facts")) {
+                String fact = catNode.get("fact").asText();
+                int length = catNode.get("length").asInt();
+                CatFact catFact = new CatFact(fact, length);
+                catInfRepository.saveCatFact(catFact);
             }
+        }
 
     }
-
-
 }
 
 
