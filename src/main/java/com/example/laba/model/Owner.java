f2d56db9b010572cd5cca13a7f7eebb8b57c41f6
egerin <zhenya.eww@gmail.com>
2024-03-17 23:17:24 +0300

    rebase new-branch

--- src/main/java/com/example/laba/model/Owner.java
+++ src/main/java/com/example/laba/model/Owner.java
@@ -1,4 +1,41 @@
 package com.example.laba.model;
 
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "owners")
+    private Set<Cat> cats = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Cat> getCats() {
+        return cats;
+    }
+
+    public void setCats(Set<Cat> cats) {
+        this.cats = cats;
+    }
 }
