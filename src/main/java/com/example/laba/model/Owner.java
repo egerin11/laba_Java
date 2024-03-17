9918a2b1b95efe55f076dd7a643e7403d3ae8f98
egerin <zhenya.eww@gmail.com>
2024-03-17 23:05:33 +0300

    fix merge_

--- src/main/java/com/example/laba/model/Owner.java
+++ src/main/java/com/example/laba/model/Owner.java
@@ -1,45 +1,4 @@
 package com.example.laba.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import jakarta.persistence.*;
-
-import java.util.HashSet;
-import java.util.Set;
-
-@Entity
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Owner {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
-
-    @ManyToMany(mappedBy = "owners")
-    private Set<Cat> cats = new HashSet<>();
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<Cat> getCats() {
-        return cats;
-    }
-
-    public void setCats(Set<Cat> cats) {
-        this.cats = cats;
-    }
 }
