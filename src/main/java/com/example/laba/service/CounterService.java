cfa93e857b58b061fc9b91c39b03a97ab1ca40c8
egerin <zhenya.eww@gmail.com>
2024-04-07 17:38:30 +0300

    fix coverage test5

--- src/main/java/com/example/laba/service/CounterService.java
+++ src/main/java/com/example/laba/service/CounterService.java
@@ -6,7 +6,13 @@ import lombok.Data;
 @Data
 public class CounterService {
 
+  private static final CounterService INSTANCE = new CounterService();
+
   private CounterService() {}
+
+  public static CounterService getInstance() {
+    return INSTANCE;
+  }
 
   private static AtomicInteger requestCount = new AtomicInteger(0);
 
--- src/test/java/com/example/laba/test_service/TestCounterService.java
+++ src/test/java/com/example/laba/test_service/TestCounterService.java
@@ -3,6 +3,8 @@ package com.example.laba.test_service;
 import static org.junit.Assert.assertEquals;
 
 import com.example.laba.service.CounterService;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class CounterServiceTest {
@@ -27,5 +29,40 @@ class CounterServiceTest {
     int currentCount = CounterService.getRequestCount();
 
     assertEquals(initialCount + 2, currentCount);
+  }
+  @Test
+  void testConcurrentIncrement() throws InterruptedException {
+    int numThreads = 10;
+    int iterations = 100;
+    int expectedCount = numThreads * iterations;
+
+
+    List<Thread> threads = new ArrayList<>();
+    for (int i = 0; i < numThreads; i++) {
+      threads.add(new Thread(() -> {
+        for (int j = 0; j < iterations; j++) {
+          CounterService.incrementRequestCount();
+        }
+      }));
+    }
+
+    threads.forEach(Thread::start);
+    for (Thread thread : threads) {
+      thread.join();
+    }
+
+    int finalCount = CounterService.getRequestCount();
+    assertEquals(expectedCount, finalCount);
+  }
+
+  @Test
+  void testSingleton() {
+    CounterService service1 = null;
+    CounterService service2 = null;
+
+    service1 = CounterService.getInstance();
+    service2 = CounterService.getInstance();
+
+    assertEquals(service1, service2);
   }
 }
